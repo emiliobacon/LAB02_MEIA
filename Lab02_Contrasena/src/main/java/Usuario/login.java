51cbbb2902c2a5855d9f5d4aa968c0da41b89e53
Emilio <eabarillasco@correo.url.edu.gt>
2022-09-12 14:45:23 -0600

    fixed global variables able to read file and assign values

--- Lab02_Contrasena/src/main/java/Usuario/login.java
+++ Lab02_Contrasena/src/main/java/Usuario/login.java
@@ -102,7 +102,7 @@ public class login extends javax.swing.JFrame {
         
         if (Password.length()< linea1)
         {
-           lblComentario.setText("La contraseña no pude tener menos de 6 caracteres..."); 
+           lblComentario.setText("La contraseña no pude tener menos de" + String.valueOf(linea1) + "caracteres..."); 
         }
         else
         {
@@ -115,7 +115,7 @@ public class login extends javax.swing.JFrame {
            lblComentario.setText(""); 
            
            //Puntuación= (Número de la segunda línea) * (largo de contraseña ingresada)
-           Puntuacion += 3*Password.length();
+           Puntuacion += linea2*Password.length();
            
            //Encontrar las mayúsculas, minúsculas, símbolos y números
             for (int i = 0; i < Password.length(); i++) {
@@ -138,55 +138,55 @@ public class login extends javax.swing.JFrame {
             //Puntuación= Puntuación + [(Número de la tercer línea)*(total de Mayúsculas ingresadas)]
             if (Mayuscula != 0) {
                 
-                Puntuacion += 2*Mayuscula;
+                Puntuacion += linea3*Mayuscula;
             }
             
             
             //Puntuación= Puntuación+ [(total de letras ingresadas)+ (Número de la cuarta línea)
             if (Mayuscula != 0 || Minuscula != 0) {
                 
-                Puntuacion += Mayuscula + Minuscula + 1;
+                Puntuacion += Mayuscula + Minuscula + linea4;
             }
             
             //Puntuación= Puntuación+ [(total de números ingresados)+ (Número de la quinta línea)
             if (Numeros != 0) {
-                Puntuacion += Numeros + 2;
+                Puntuacion += Numeros + linea5;
             }
             
             //Puntuación= Puntuación + (total de símbolos ingresados) *[(largo de cadena) + (Número de la sexta línea) [/¿?%$#]
             if (Simbolos != 0) {
                 
-                Puntuacion += Simbolos * (Password.length() + 4);
+                Puntuacion += Simbolos * (Password.length() + linea6);
             }         
             
             //Si hay sólo letras: Puntuación – (Número de la séptima línea)
             if (Numeros == 0 && Simbolos == 0) {
                 
-                Puntuacion -= 6;
+                Puntuacion -= linea7;
             }
             
             //Si hay sólo números: Puntuación – (Número de la octava línea)
             
             if (Mayuscula == 0 && Minuscula == 0 && Simbolos == 0) {
                 
-                Puntuacion -= 3;
+                Puntuacion -= linea8;
             }
             
             //Imprimir comentario sobre seguridad de contraseña
             
-            if (Puntuacion >= 0 && Puntuacion <= 25) {
+            if (Puntuacion >= inf1 && Puntuacion <= sup1) {
                 
                 lblComentario.setText("Contraseña Insegura...");
             }
-            else if (Puntuacion >= 26 && Puntuacion <= 35) {
+            else if (Puntuacion >= inf2 && Puntuacion <= sup2) {
                 
                 lblComentario.setText("Contraseña poco Segura...");
             }
-            else if (Puntuacion >= 36 && Puntuacion <= 50) {
+            else if (Puntuacion >= inf3 && Puntuacion <= sup3) {
                 
                 lblComentario.setText("Contraseña Segura...");
             }
-            else if (Puntuacion >= 51) {
+            else if (Puntuacion >= inf4 && Puntuacion <= sup4) {
                 
                 lblComentario.setText("Contraseña muy Segura...");
             }
